96858dc40e8d82d79a2c141629762e88204be18f
AgustinIbanez00 <48695094+AgustinIbanez00@users.noreply.github.com>
2019-11-03 23:27:12 -0300

    Main Activity

--- app/src/main/java/com/agustin/prcticointegrador_apirest/request/ApiClient.java
+++ app/src/main/java/com/agustin/prcticointegrador_apirest/request/ApiClient.java
@@ -8,7 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
 
 public class ApiClient {
-    private static final String PATH = "https://apis.datos.gob.ar/georef/api/";
+    private static final String PATH = "https://www.cultura.gob.ar/api/v2.0/museos/";
     private static MyApiInterface myApiInterface;
 
     public static MyApiInterface getMyApiInterface()
