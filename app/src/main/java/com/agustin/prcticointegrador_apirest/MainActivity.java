2a33f417fedc3d9c24d6644e35f641bc26692850
AgustinIbanez00 <48695094+AgustinIbanez00@users.noreply.github.com>
2019-11-05 16:22:19 -0300

    Activity Main change

--- app/src/main/java/com/agustin/prcticointegrador_apirest/MainActivity.java
+++ app/src/main/java/com/agustin/prcticointegrador_apirest/MainActivity.java
@@ -3,14 +3,33 @@ package com.agustin.prcticointegrador_apirest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvBuscar;
+    ListView lvResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        configView();
+
+    }
+
+    void configView()
+    {
+        tvBuscar = findViewById(R.id.tvBuscar);
+        lvResultado = findViewById(R.id.lvResultado);
+    }
+
+    void buscar(View view)
+    {
 
 
     }
+
 }
